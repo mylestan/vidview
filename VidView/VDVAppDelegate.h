cd5605532fcd08c7e79e21b96873a4970b2dddac
Myles Tan <mylestan7@gmail.com>
2013-04-27 12:17:46 +0800

    Initial Commit

---
+++ VidView/VDVAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VDVAppDelegate.h
+//  VidView
+//
+//  Created by Myles Tan on 13-04-27.
+//  Copyright (c) 2013 Myles Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VDVAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VidView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  VidView
+//
+//  Created by Myles Tan on 13-04-27.
+//  Copyright (c) 2013 Myles Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "VDVAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([VDVAppDelegate class]));
+    }
+}
